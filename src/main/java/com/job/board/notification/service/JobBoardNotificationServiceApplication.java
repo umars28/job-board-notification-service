cdb842b946416bdcf05cbd448695c5ac59f39e10
Umar <umarsabirin369@gmail.com>
2025-06-21 14:09:23 +0800

    Init project for notification service :Job Board

---
+++ src/main/java/com/job/board/notification/service/JobBoardNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.job.board.notification.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBoardNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBoardNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/job/board/notification/service/JobBoardNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job.board.notification.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBoardNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
